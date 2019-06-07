e1575e4f81ab62734de2287cac6d63de6176aa7f
Bud Welsh <budwelsh14@gmail.com>
2019-06-07 05:44:46 -0500

    Add a string by calling a variable

--- stringPractice.py
+++ stringPractice.py
@@ -2,4 +2,7 @@ print("Good Times") # A regular string
 
 print("A\nnew\nline") # A new line
 
-print("\"Escaping quotes\"") 
+print("\"Escaping quotes\"")
+
+phrase = "This string comes from the variable \"phrase\"."
+print(phrase)
