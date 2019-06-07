e73cc1dd856543dca19688ac188d8c81044133df
Bud Welsh <budwelsh14@gmail.com>
2019-06-07 06:23:16 -0500

    Find letter with index number and find the index with letters or words

--- stringPractice.py
+++ stringPractice.py
@@ -12,3 +12,9 @@ print(phrase.lower()) # Make phrase all lower case letters
 print(phrase.upper()) # Make phrase all upper case letters
 
 print(len(phrase)) # checking the length of the phrase
+
+print(phrase[0]) # printing the first letter of the phrase
+print(phrase[3]) # printing the fourth letter of the phrase
+
+print(phrase.index("m")) # find the index of the letter m in the phrase
+print(phrase.index("the")) # find the starting index of the word the in the phrase
