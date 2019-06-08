2eb66301cff193c73a5afe6f5f3c474beef7f10c
Bud Welsh <budwelsh14@gmail.com>
2019-06-08 06:00:35 -0500

    Add while loop to add numbers from 1 to input

--- elementaryProblems.py
+++ elementaryProblems.py
@@ -5,3 +5,12 @@ if name == "Alice" or name == "Bob":
   print("Hello, " + name)
 else:
   print("Hello, user")
+
+var_number = input("Give me a number: ")
+i = 0
+sum_number = 0
+while i <= var_number:
+  sum_number = sum_number + i
+  i += 1
+print("The sum of the numbers from 1 to the number you gave me is: ")
+print(sum_number)
