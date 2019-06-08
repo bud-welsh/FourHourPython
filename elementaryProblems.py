6076749fed3ec29ac5c3904bbe0c0392f676dbba
Bud Welsh <budwelsh14@gmail.com>
2019-06-08 05:21:25 -0500

    Modified Hello User to check for Alice or Bob

--- elementaryProblems.py
+++ elementaryProblems.py
@@ -1,4 +1,7 @@
 print("Hello World")
 
 name = raw_input("What's your name? ")
-print("Hello, " + name)
+if name == "Alice" or name == "Bob":
+  print("Hello, " + name)
+else:
+  print("Hello, user")
