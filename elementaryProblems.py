8ef0f644260b2411e1a91ef87ebac52c400ca0f4
Bud Welsh <budwelsh14@gmail.com>
2019-06-10 06:52:43 -0500

    Add difference of two numbers

--- elementaryProblems.py
+++ elementaryProblems.py
@@ -27,5 +27,8 @@ print(multi_number)
 a = input("Give me a number: ")
 b = input("Give me another number: ")
 a_b_sum = a + b
+a_b_dif = a - b
 print("The sum of those two numbers is:")
 print(a_b_sum)
+print("The difference of those two numbers is:")
+print(a_b_dif)
