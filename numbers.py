3dd84c75f2676b4ddf1a293eaefd44d276dfde36
Bud Welsh <budwelsh14@gmail.com>
2019-06-11 06:22:12 -0500

    Add order of operation and mod to file

--- numbers.py
+++ numbers.py
@@ -4,3 +4,6 @@ print(2 + 2) # Add 2 numbers
 print(6 - 3) # Subtract 2 numbers
 print(3 * 9) # Multiply 2 numbers
 print(8 / 2) # Divide 2 numbers
+
+print(3 * (4 + 5)) # Order of operation should be 27
+print(10 % 3) # Modulation should return the remainder of 1
