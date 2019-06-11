e462b52334ee496bf215e35fcc864939be39b1d5
Bud Welsh <budwelsh14@gmail.com>
2019-06-11 06:28:42 -0500

    Make an integer into a string

--- numbers.py
+++ numbers.py
@@ -8,5 +8,6 @@ print(8 / 2) # Divide 2 numbers
 print(3 * (4 + 5)) # Order of operation should be 27
 print(10 % 3) # Modulation should return the remainder of 1
 
-my_num = 5 # making a variable
+my_num = 421 # making a variable
 print(my_num)
+print("My favorite number is " + str(my_num))
