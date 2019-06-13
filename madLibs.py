44618869abd0d450bff7f1ab5afbf8b1af28ef13
Bud Welsh <budwelsh14@gmail.com>
2019-06-13 14:34:48 -0500

    Add variables and variables into poem

--- madLibs.py
+++ madLibs.py
@@ -1,7 +1,14 @@
 # Thursday, June 13, 2019
 # Mad Libs Game from tutorial
 
-print("Roses are red")
-print("Violets are blue")
-print("This poem really sucks")
-print("And so do you")
+# Make some variables and fill them with user input
+color = raw_input("Enter a color: ")
+plural_noun = raw_input("Enter a plural noun: ")
+verb = raw_input("Enter a verb: ")
+celebrity = raw_input("Enter a celebrity's name: ")
+
+# Wrote this sweet poem and changed some of the words to inputs
+print("Roses are " + color)
+print(plural_noun + " are blue")
+print("This poem really " + verb)
+print("And so does " + celebrity)
