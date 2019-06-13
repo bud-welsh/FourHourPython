6cb64ff7801dcb83be569368c0af92065c7e9ed9
Bud Welsh <budwelsh14@gmail.com>
2019-06-13 14:06:09 -0500

    Add input of two variables and add them together and print result

--- calculator.py
+++ calculator.py
@@ -1,3 +1,9 @@
 # Thursday, June 13, 2019
 # Building a calculator from a tutorial
 
+# Variables to put into the calculator
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+result = num1 + num2
+
+print(result)
