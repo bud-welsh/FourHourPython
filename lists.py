579c9dace0aea2adcf71707839bc1e955a46b9ee
Bud Welsh <budwelsh14@gmail.com>
2019-06-14 20:32:58 -0500

    Change Peter to Eric

--- lists.py
+++ lists.py
@@ -6,3 +6,11 @@ kiss = ["Paul", "Gene", "Peter", "Ace"]
 
 # Print Array
 print(kiss)
+# Print Ace
+print(kiss[3])
+# Print Ace again
+print(kiss[-1])
+# Change Peter to Eric in array
+kiss[2] = "Eric"
+print(kiss[2])
+print(kiss)
