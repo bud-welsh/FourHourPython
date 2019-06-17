9f5246ef9c58f4adc38918860d49b6e724c05d0a
Bud Welsh <budwelsh14@gmail.com>
2019-06-16 19:37:02 -0500

    Add function

--- leapYear.py
+++ leapYear.py
@@ -2,3 +2,13 @@
 # Return whether or not that is a leap year
 
 year = input("Give me a year: ")
+
+def is_leap(year):
+  if year % 4 == 0:
+    if year % 100 == 0 and year % 400 != 0:
+      leap = False
+    else:
+      leap = True
+
+    return leap
+
